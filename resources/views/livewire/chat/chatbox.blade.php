471c53c8e18f36ad050420559dd869df83db6c98
Naruse Chiho <penyo6697@gmail.com>
2023-01-30 22:25:12 +0900

    Add Chat-Box view

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,13 +1,54 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <div class="chat_box_header">
-        header
+        <div class="return">
+            <i class="bi bi-arrow-left"></i>
+        </div>
+        <div class="img_container">
+            <img src="https://picsum.photos/id/231/200/300" alt="">
+        </div>
+
+        <div class="name">
+            john
+        </div>
+        <div class="info">
+            <div class="info_item">
+                <i class="bi bi-telephone-fill"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-image"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-info-square-fill"></i>
+            </div>
+
+        </div>
     </div>
     <div class="chat_box_body">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugit. Vel, earum. Facilis odit dolores temporibus dolor aperiam et quidem architecto accusantium perferendis quibusdam eum, eaque distinctio, quasi itaque asperiores.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae reiciendis ducimus officiis optio hic voluptatibus, numquam inventore aspernatur dolor exercitationem possimus, unde ipsum iusto eaque? Et, exercitationem nesciunt. Voluptate, repellat.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, quas voluptatem autem pariatur illum debitis, beatae, alias incidunt accusantium minus reiciendis. Laudantium nulla quisquam fugiat vel adipisci praesentium alias fugit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi necessitatibus incidunt eaque at quasi in molestias iste ducimus? Delectus veniam possimus earum blanditiis ratione quidem officiis natus vel itaque? Tenetur?
+        <div class="msg_body msg_body_receiver">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sequi nam velit ea sed odio molestias possimus ad praesentium. Repudiandae quo inventore tenetur sed odit porro iure harum! Sint, expedita.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi velit explicabo obcaecati sed sit, facere distinctio ea quos quis, itaque voluptates excepturi, ex reprehenderit quas sapiente aliquid. Tempora, sequi suscipit.
+            <div class="msg_body_footer">
+                <div class="date">
+                    5 hours ago
+                </div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
+        <div class="msg_body msg_body_me">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sequi nam velit ea sed odio molestias possimus ad praesentium. Repudiandae quo inventore tenetur sed odit porro iure harum! Sint, expedita.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi velit explicabo obcaecati sed sit, facere distinctio ea quos quis, itaque voluptates excepturi, ex reprehenderit quas sapiente aliquid. Tempora, sequi suscipit.
+            <div class="msg_body_footer">
+                <div class="date">
+                    5 hours ago
+                </div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="chat_box_footer">
         footer
