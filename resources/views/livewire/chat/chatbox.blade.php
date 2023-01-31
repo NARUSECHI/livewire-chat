2e2231930a7e52556997a6dcf1ea4610ba51e9da
Naruse Chiho <penyo6697@gmail.com>
2023-01-31 22:58:48 +0900

    Add Chat-Footer view

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -49,8 +49,17 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="chat_box_footer">
-        footer
+        <div class="msg_body msg_body_me">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sequi nam velit ea sed odio molestias possimus ad praesentium. Repudiandae quo inventore tenetur sed odit porro iure harum! Sint, expedita.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi velit explicabo obcaecati sed sit, facere distinctio ea quos quis, itaque voluptates excepturi, ex reprehenderit quas sapiente aliquid. Tempora, sequi suscipit.
+            <div class="msg_body_footer">
+                <div class="date">
+                    5 hours ago
+                </div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/livewire/chat/message.blade.php
+++ resources/views/livewire/chat/message.blade.php
@@ -1,6 +1,15 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
     <div class="chat_box_footer">
+        <form action="" method="post">
+            @csrf
+            <div class="custom_form_group">
 
+
+            <input type="text" name="message" id="message" class="control" placeholder="Write message ...">
+            <submit type="submit" class="submit">Send</submit>
+            </div>
+            
+        </form>
     </div>
 </div>
